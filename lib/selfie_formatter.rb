1af07fbd639c339f192515fc8a1da5c104a32a1c
sshaw <skye.shaw@gmail.com>
2016-06-29 23:50:12 -0400

    Make sure unsupported terminal warnings are seen

--- lib/selfie_formatter.rb
+++ lib/selfie_formatter.rb
@@ -11,7 +11,25 @@ else
   require "rspec/core/formatters"
 
   SelfieFormatter = Class.new(RSpec::Core::Formatters::ProgressFormatter) do
+    # We output the warning twice: once for those will slow specs and another for those with fast
+    def start(n)
+      super
+      output_warning
+    end
+
     def dump_summary(summary)
+      output_warning
+      super
+    end
+
+    # Defined just to meet Selfie::Formatter registration requirements below
+    def stop(n)
+      super if defined?(super) # For forward compatibility :)
+    end
+
+    private
+
+    def output_warning
       warn <<WARNING
 
 SelfieFormatter Warning:
@@ -20,13 +38,6 @@ Unsupported terminal. You must use iTerm2 >= v3.0.
 Since your terminal is not supported, the progress formatter was used in place of the SelfieFormatter.
 
 WARNING
-
-      super
-    end
-
-    # Defined just to meet Selfie::Formatter registration requirements below
-    def stop(n)
-      super if defined?(super) # For forward compatibility :)
     end
   end
 end
